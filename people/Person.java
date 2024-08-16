7efd07cf82dac3ea977296b0401f4f73cd71be58
Zuly <zurojasco@est.utn.ac.cr>
2024-08-16 16:12:21 -0600

    Clase Custumer
    Se modifica la clase custumer agregandole los atributos y constructores correspondientes

---
+++ people/Person.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package people;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author zulay
+ */
+ public class Person {
+  private String id;
+  private String name;
+  private LocalDate birthDate;
+  private String phone;
+  private String email;
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Person(String id, String name, LocalDate birthDate, String phone, String email) {
+    this.id = id;
+    this.name = name;
+    this.birthDate = birthDate;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public Person() {
+    this("", "", null, "", "");
+  }
+
+  @Override
+  public String toString() {
+    return "id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", phone=" + phone + ", email=" + email;
+  }
+}
