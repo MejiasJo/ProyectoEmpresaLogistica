30b8879558f2415079ac6b1913490b486bd4fabf
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2024-08-17 21:55:34 -0600

    Se creo la Clase Rount y DestinationList
    Rount donde estan sus atributos, metodos y constructores
    
    DestinationList  administra las rutas donde se enviaran los paquetes

---
+++ routes/Listdesnation.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package routes;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author DannyTini
+ */
+public class Listdesnation extends Route {
+
+    private HashSet<Route> routes;
+
+    public Listdesnation() {
+        routes = new HashSet<>();
+    }
+    
+    public void addRount(String Rount) {
+        destinationList.add(Rount);
+    }
+
+    public void deleteRoute(int code) {
+        for (Route route : routes) {
+            if (route.getCode() == code) {
+                routes.remove(route);
+                break;
+            }
+        }
+    }
+    public Route searchRoute(int code) {
+        for (Route route : routes) {
+            if (route.getCode() == code) {
+                return route;
+            }
+        }
+        return null;
+    }
+}
---
+++ routes/Route.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package routes;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author DaniTini
+ */
+public class Route {
+
+    private static int count = 1;
+    private int code;
+    private String name;
+    private String description;
+    HashSet<String> destinationList;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public HashSet<String> getDestinationList() {
+        return destinationList;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDestinationList(HashSet<String> destinationList) {
+        this.destinationList = destinationList;
+    }
+
+    public Route(String name, String description) {
+        this.code = count++;
+        this.name = name;
+        this.description = description;
+        this.destinationList = new HashSet();
+
+    }
+
+    public Route() {
+        this("", "");
+    }
+
+    public void addDestiny(String destiny) {
+        destinationList.add(destiny);
+    }
+
+    public String searchDestiny(String destiny) {
+        if (destiny != null) {
+            for (String Destiny : destinationList) {
+                if (Destiny.equals(destiny)) {
+                    return destiny;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" + "code=" + code + ", name=" + name + ", description=" + description + ", destinationList=" + destinationList.toString() + '}';
+    }
+
+}
