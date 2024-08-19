718344475af26bc312a2af73455d6d3a525ecff6
Zuly <zurojasco@est.utn.ac.cr>
2024-08-18 20:00:11 -0600

    Creacion de la clase EmployeeList
    Se crea la clase employee list con la finalidad de almacenar a los empleados en un ArrayList para mayor facilidad, ademas se crean sus respectivos metodos

---
+++ employees/EmployeeList.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employees;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author zulay
+ */
+public class EmployeeList {
+     private List<Employee> employees;
+
+    public EmployeeList() {
+        employees = new ArrayList<>();
+    }
+
+    //Agregar
+    public void addEmployee(String id, String name, LocalDate birthDate, String phone, String email, String position, double salary) {
+        Employee newEmployee = new Employee(position, salary, id, name, birthDate, phone, email);
+        employees.add(newEmployee);
+    }
+    
+    //actualizar
+    public boolean updateEmployee(String id, String newPhone, String newEmail, String newPosition, double newSalary) {
+        Employee employee = findEmployeeById(id);
+        if (employee != null) {
+            employee.setPhone(newPhone);
+            employee.setEmail(newEmail);
+            employee.setPosition(newPosition, newSalary);
+            return true;
+        }
+        return false;
+    }
+    
+    //eliminar por su id
+    public boolean removeEmployee(String id) {
+        Employee employee = findEmployeeById(id);
+        if (employee != null) {
+            employees.remove(employee);
+            return true;
+        }
+        return false;
+    }
+     
+    //Buscar por su id
+    public Employee findEmployeeById(String id) {
+        for (Employee employee : employees) {
+            if (employee.getId().equals(id)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    public void listAllEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
